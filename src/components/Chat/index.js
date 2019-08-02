e0a9541e37b8f0d6fe7cec9387185996cc8375d5
Mihail <mihail.pogosyan96@gmail.com>
2019-08-02 22:29:57 +0300

    add autoscroll and check empty sumbit

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -8,26 +8,49 @@ class Chat extends React.Component {
     messageInput: ''
   };
 
-  changeInputMessage = event => {
-    const val = event.target.value
-    this.setState({...this.state, messageInput: val})
+  scrollToBottom() {
+    const scrollHeight = this.refs.messageList.scrollHeight;
+    const height = this.refs.messageList.clientHeight;
+    const maxScrollTop = scrollHeight - height;
+    this.refs.messageList.scrollTop = maxScrollTop > 0 ? maxScrollTop : 0;
   }
+
+  changeInputMessage = event => {
+    this.setState({ messageInput: event.target.value });
+  };
 
   sendMessageOnEnter = event => {
     if (event.key === 'Enter') {
-      this.setState({ messages: [...this.state.messages, { text: this.state.messageInput }], messageInput: '' })
+      this.state.messageInput &&
+        this.setState(
+          {
+            messages: [
+              ...this.state.messages,
+              { text: this.state.messageInput }
+            ],
+            messageInput: ''
+          },
+          () => this.scrollToBottom()
+        );
     }
-  }
+  };
 
   render() {
     return (
       <div className="chat">
-        <div className="message-list">
+        <div className="message-list" ref="messageList">
           <div className="messages">
-            {this.state.messages.map((msg, i) => (<Message text={msg.text} key={i}/>))}
+            {this.state.messages.map((msg, i) => (
+              <Message text={msg.text} key={i} />
+            ))}
           </div>
         </div>
-        <input className="input-message" value={this.state.messageInput} onChange={this.changeInputMessage} onKeyPress={this.sendMessageOnEnter}/>
+        <input
+          className="input-message"
+          value={this.state.messageInput}
+          onChange={this.changeInputMessage}
+          onKeyPress={this.sendMessageOnEnter}
+        />
       </div>
     );
   }
